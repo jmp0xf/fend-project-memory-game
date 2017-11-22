3417e63467d3e1eb6e91137e092191be9776526b
jmp0xf <jmp0xf@gmail.com>
2017-11-22 17:06:31 +0800

    feature: implement game reset function

--- js/app.js
+++ js/app.js
@@ -70,3 +70,7 @@ function resetDeck(cardList) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function resetGame() {
+    resetDeck(cardList);
+}
